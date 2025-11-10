387015dc1ca4befdeeab739da61d6b9657f8bd5d
Adrian <neacsu_adrian@yahoo.com>
2025-11-10 18:48:07 +0200

    B2

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.kt
@@ -1,12 +1,19 @@
 package ro.pub.cs.systems.eim.practicaltest01var04
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PracticalTest01Var04MainActivity : AppCompatActivity() {
+
+    private lateinit var input1: EditText
+    private lateinit var input2: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,23 @@ class PracticalTest01Var04MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        input1 = findViewById(R.id.input1)
+        input2 = findViewById(R.id.input2)
+        input1.setText("Perfect Student")
+        input2.setText("341C1")
+
+        val info = findViewById<TextView>(R.id.info)
+        val display = findViewById<Button>(R.id.display)
+        display.setOnClickListener {
+            var inp = "" + input1.text.toString()
+            if (input1.text.toString() == "" && input1.text.toString() == "") {
+                Toast.makeText(applicationContext,"Nu merge"
+                    , Toast.LENGTH_LONG).show()
+            } else {
+                info.setText(inp)
+            }
+        }
+
     }
 }
